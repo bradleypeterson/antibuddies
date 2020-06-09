de6e5ca528a00e84d6e8446b052c2dcb0560374e
Constance Timmerman <ctimmerman@mail.weber.edu>
2020-06-08 20:02:04 -0600

    Added node components to @NGModule declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,23 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { TeachHeaderComponent } from './Teacher_Header/teacher-header.component';
 import { LabListComponent } from './Lab-List/lab-list.component';
+import { TreeNodeComponent } from './tree-node/tree-node.component';
+import { QuizNodeComponent } from './quiz-node/quiz-node.component';
+import { MatchingNodeComponent } from './matching-node/matching-node.component';
+import { ExperimentNodeComponent } from './experiment-node/experiment-node.component';
+import { VideoNodeComponent } from './video-node/video-node.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     TeachHeaderComponent,
-    LabListComponent
+    LabListComponent,
+    TreeNodeComponent,
+    MatchingNodeComponent,
+    VideoNodeComponent,
+    ExperimentNodeComponent,
+    QuizNodeComponent,
   ],
   imports: [
     BrowserModule
