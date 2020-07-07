38a1ee3b1aca79365a6d3750b8ce44b0c4d7394d
Constance Timmerman <ctimmerman@mail.weber.edu>
2020-07-07 10:50:54 -0600

    Student Lab component working and accessible from main lab view. Nearly complete (with dummy data)

--- virtual lab/src/app/student/student-lab/student-lab.component.ts
+++ virtual lab/src/app/student/student-lab/student-lab.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { BackEndServiceService } from '../../back-end-service.service'
+import { BackEndServiceService } from '../../back-end-service.service';
+import { MessageService } from '../../message.service';
 //import { labType, quizNode } from '../../interfaces';
 
 @Component({
@@ -12,7 +13,9 @@ export class StudentLabComponent implements OnInit {
   isBegin: boolean = false;
   currentNode: number = 0;
   nextNode: number;
+  prevNode: number;
   disabledNext: string = "disabled";
+  disabledPrevious: string = "disabled";
   dummyLab = {
       // dummy lab - labId, name, description, publishDate, course,
       // nodes array
@@ -67,47 +70,153 @@ export class StudentLabComponent implements OnInit {
             // populate with dummy answers
             {
               answerId: 0,
-              nextNodeId: 1,
-              answerText: "This first option is correct",
+              nextNodeId: 5,
+              answerText: "Follow me to the end!",
             },
             {
               answerId: 1,
-              nextNodeId: 2,
+              nextNodeId: 6,
               answerText: "This second option is correct"
             },
             {
               answerId: 2,
-              nextNodeId: 3,
+              nextNodeId: 7,
               answerText: "This third option is the right one"
             }
           ]
-        }
+        },
+        {
+          nodeId: 2,
+          nodeType: 'quiz',
+          question: "This is the third question (nodeId: 2)",
+          answers: [
+            // populate with dummy answers
+            {
+              answerId: 0,
+              nextNodeId: 8,
+              answerText: "cupcakes",
+            },
+            {
+              answerId: 1,
+              nextNodeId: 9,
+              answerText: "rainbows"
+            },
+            {
+              answerId: 2,
+              nextNodeId: 10,
+              answerText: "sprinkles"
+            },
+            {
+              answerId: 3,
+              nextNodeId: 11,
+              answerText: "butterflies"
+            }
+          ]
+        },
+        {
+          nodeId: 3,
+          nodeType: 'quiz',
+          question: "This is the fourth question (nodeId: 3)",
+          answers: [
+            // populate with dummy answers
+            {
+              answerId: 0,
+              nextNodeId: 12,
+              answerText: "Fourth node answer 1",
+            },
+            {
+              answerId: 1,
+              nextNodeId: 13,
+              answerText: "Another dummy answer for you"
+            },
+            {
+              answerId: 2,
+              nextNodeId: 14,
+              answerText: "Pick me!"
+            },
+            {
+              answerId: 3,
+              nextNodeId: 15,
+              answerText: "Arbitrary nonsense"
+            }
+          ]
+        },
+        {
+          nodeId: 5,
+          nodeType: 'quiz',
+          question: "Quick, answer this final question! (nodeId: 5)",
+          answers: [
+            // populate with dummy answers
+            {
+              answerId: 0,
+              nextNodeId: 16,
+              answerText: "Click me and be done with the lab already!!",
+            },
+            {
+              answerId: 1,
+              nextNodeId: 17,
+              answerText: "This second option is correct"
+            },
+            {
+              answerId: 2,
+              nextNodeId: 18,
+              answerText: "This third option is the right one"
+            }
+          ]
+        },
       ]
   };
   
   // Actual lab for use later
   //newLab: labType;
 
-  constructor(private backEnd: BackEndServiceService) { 
+  constructor(private backEnd: BackEndServiceService, private messages: MessageService) { 
     // Retrieve lab from service - test
     // this.newLab = backEnd.getLabID(0);
   }
 
   ngOnInit(): void {
+    this.messages.add('Student labview page loaded');
   }
 
   handleBegin(): void {
-    this.isBegin = true
+    this.isBegin = true;
+  }
+
+  getNextIndex(next: number): number {
+    // returns array index of node with specific ID
+    // console.log("getNextIndex: index of next node: ", this.dummyLab.nodes.findIndex((element) => element.nodeId === next))
+    return this.dummyLab.nodes.findIndex((element) => element.nodeId === next)
   }
 
   goNextNode(next: number): void {
-    this.nextNode = next;
-    this.disabledNext = ""
+    // check that node exists before allowing traversal
+    if (this.getNextIndex(next) !== -1) {
+      this.nextNode = this.getNextIndex(next);
+      this.disabledNext = "";
+    } else {
+      this.disabledNext = "disabled";
+    }
   }
 
-  // function to handle future node traversal functionality
-  handleTraverse(): void {
+  // method to handle forward tree traversal
+  handleTraverseForward(): void {
+    this.prevNode = this.currentNode;
+    this.currentNode = this.nextNode;
+    this.disabledNext = "disabled";
+    this.disabledPrevious = "";
+  }
 
+  // method to handle backward tree traversal --
+  // currently this will only work for one node, since having multiple
+  // previous nodes stored would require a structure (like an array)
+  // to store the previous nodes for traversal. Currently
+  // it is only a variable with a single value.
+  handleTraverseBackward(): void {
+    // this.prevNode
+    this.currentNode = this.prevNode;
+    this.disabledNext = "";
+    this.disabledPrevious = "disabled";
   }
 
 }
--- virtual lab/src/app/student/student-lab/student-toolbar/student-toolbar.component.ts
+++ virtual lab/src/app/student/student-lab/student-toolbar/student-toolbar.component.ts
@@ -11,7 +11,9 @@ export class StudentToolbarComponent implements OnInit, OnChanges {
 // so that the next node button is no longer greyed out,
 // allow for traversal to the next node
 @Input() allowNext: string;
-@Output() traverseIsOkay: EventEmitter<Boolean> = new EventEmitter<Boolean>();
+@Input() allowPrevious: string;
+@Output() traverseForwardIsOkay: EventEmitter<Boolean> = new EventEmitter<Boolean>();
+@Output() traverseBackwardIsOkay: EventEmitter<Boolean> = new EventEmitter<Boolean>();
 
   currNodeType: string = 'Play video';
 
@@ -20,12 +22,13 @@ export class StudentToolbarComponent implements OnInit, OnChanges {
   handlePreviousNode(): void {
     // go to previous node -- load previous node view
     console.log("Go to prev node");
+    this.traverseBackwardIsOkay.emit(true);
   }
 
   handleNextNode(): void {
     // go to next node -- load next node view
     console.log("Go to next node");
-    this.traverseIsOkay.emit(true);
+    this.traverseForwardIsOkay.emit(true);
 
   }
 
@@ -35,7 +38,6 @@ export class StudentToolbarComponent implements OnInit, OnChanges {
   }
 
   ngOnInit(): void {
-
   }
 
   ngOnChanges(): void {
